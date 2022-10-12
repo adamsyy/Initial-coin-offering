7f6b9f6f57c531b18e66670380924d587f957d17
adamsyy <adamoommenjacob@gmail.com>
2022-10-13 02:28:42 +0530

    changed envs

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x567C7F370BEb5febFda89bDCE0CaaE3EC86c1F26";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
